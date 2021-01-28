7ba2212817b21116e0c82d1cd4f25e533547a4ff
Fabien Donnet <fabien.donnet@ubiko.ch>
2021-01-28 13:14:50 +0100

    better copy and convert sh script => file are copied for conversion only if not exist

--- prestawebp.sh
+++ prestawebp.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
+# Get all jpeg files
 cd /prestawebsitefolder/img
-find . -name '*.jpg' -exec cp --parents \{\} /prestawebsitefolder/imgwebp \;
-cd /prestawebsitefolder/imgwebp
-bmp_files=$(find . -iname "*.jpg")
+jpg_files=$(find . -iname "*.jpg")
 
-total=$(echo "$bmp_files" | wc -l)
-num=0
-
-echo "There are $total files to be converted."
-
-for f in $bmp_files
+#Loop on old jpeg files
+for jpg_file in $jpg_files
 do
-    ((num++))
-    
-    echo "$f"
-    if [ ! -f "${f%.jpg}.webp" ]; then
-     cwebp -q 80 "$f" -o "${f%.jpg}.webp"
+    #Check if the webp already exists before copy
+    #echo "Old file path: $jpg_file"
+
+    webp_target_file="/prestawebsitefolder/imgwebp/${jpg_file%.jpg}.webp"
+    #echo "Target file path: $webp_target_file"
+    if [ ! -f "$webp_target_file" ]; then
+        #echo "File doesn't exist, proceed with the copy : $webp_target_file"
+        cp --parents $jpg_file /prestawebsitefolder/imgwebp
     fi
-     rm "$f"
 done
+
+#Go in wepp directory
+cd /prestawebsitefolder/imgwebp
+copied_jpg_files=$(find . -iname "*.jpg")
+
+#Loop on copied jpg files in webp directory to convert
+for new_jpg in $copied_jpg_files
+do
+    cwebp -q 100 "$new_jpg" -o "${new_jpg%.jpg}.webp" -quiet
+    echo "File ${new_jpg%.jpg}.webp created !"
+    rm "$new_jpg"
+    #echo "Odl file $new_jpg removed !"
+done
+
+#Change the owner of the new created files
 chown -R www-data:www-data /prestawebsitefolder/imgwebp
